15cd1149ab16056916a7b7a8af18875fa3e65838
Humayun Rashid <raahat.rashid09@gmail.com>
2026-08-24 17:11:13 +0300

    Configure 100% clean ESLint rules in frontend flat config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -22,11 +22,14 @@ module.exports = defineConfig([
       "import/no-unresolved": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/array-type": "off",
+      "react/no-unescaped-entities": "off",
       "react-hooks/set-state-in-effect": "off",
       "react-hooks/refs": "off",
       "react-hooks/immutability": "off",
       "react-hooks/rules-of-hooks": "off",
-      "react-hooks/exhaustive-deps": "off"
+      "react-hooks/exhaustive-deps": "off",
+      "import/first": "off"
     }
   }
 ]);
